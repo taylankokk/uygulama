f29ea99dc1f2c46a75381697fbc89173de720154
taylan <taylankok9595@gmail.com>
2025-09-23 13:03:34 +0300

    first commit

---
+++ JİlkKonsolUyg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Burada\t ");
+        Console.WriteLine("Merhaba C Sharp Programlama");
+        //Ekrana yazı yazdırma
+        /*Ekran */
+        string insan = "Taylan";
+        Console.WriteLine("Hoşgeldin " +insan);
+    }
+}
